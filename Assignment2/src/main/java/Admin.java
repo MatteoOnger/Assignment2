2870b773ea76cae24dd23a6d58b8f4d69aa82f21
MatteoOnger <>
2021-10-31 19:06:14 +0100

    New attribute.

--- Assignment2/src/main/java/Admin.java
+++ Assignment2/src/main/java/Admin.java
@@ -3,9 +3,14 @@ import java.util.List;
 public class Admin extends User {
 	
 	private String password;
-	private List<String> sessionsOwned;
+	private List<String> currentSessionsOwned;
+	private List<String> oldsessionsOwned;
 	
-	public List<String> getSessionsOwned() {
-		return this.sessionsOwned;
-	} 	
+	public List<String> getCurrentSessionsOwned() {
+		return currentSessionsOwned;
+	}
+	
+	public List<String> getOldsessionsOwned() {
+		return oldsessionsOwned;
+	}
 }
